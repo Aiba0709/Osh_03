6e289061da2a554dff069d191516f10e624340b4
Aibek <baimuratovaibek01@mail.com>
2022-11-16 23:08:09 +0300

    first commit

---
+++ Homework1/main.py
@@ -0,0 +1,13 @@
+class person():
+    def __init__(self, fullname, age, is_married):
+        self.fullname == fullname
+        self.age == age
+        self.is_married == is_married
+
+    def info(self):
+        return (f"фамилия {self.fuiiname}  возрасть {self.age} семейное положение {self.is_married} ")  
+        
+
+# person1 = person("Баймуратов", 27, "Холост")
+
+print(person.info)
